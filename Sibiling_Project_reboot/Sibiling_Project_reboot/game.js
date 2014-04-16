0af56f0aebde1f6cdf9ba77efeb36e2213d7ec0d
Thomas Nunn <tmnunn7@uw.edu>
2014-04-16 14:39:58 -0700

    Tom commit 2

--- Sibiling_Project_reboot/Sibiling_Project_reboot/game.js
+++ Sibiling_Project_reboot/Sibiling_Project_reboot/game.js
@@ -13,6 +13,407 @@ window.requestAnimFrame = (function () {
             };
 })();
 
+function AssetManager() {
+    this.successCount = 0;
+    this.errorCount = 0;
+    this.cache = [];
+    this.downloadQueue = [];
+}
+
+AssetManager.prototype.queueDownload = function (path) {
+    console.log(path.toString());
+    this.downloadQueue.push(path);
+}
+
+AssetManager.prototype.isDone = function () {
+    return (this.downloadQueue.length == this.successCount + this.errorCount);
+}
+AssetManager.prototype.downloadAll = function (callback) {
+    if (this.downloadQueue.length === 0) window.setTimeout(callback, 100);
+    for (var i = 0; i < this.downloadQueue.length; i++) {
+        var path = this.downloadQueue[i];
+        var img = new Image();
+        var that = this;
+        img.addEventListener("load", function () {
+            console.log("dun: " + this.src.toString());
+            that.successCount += 1;
+            if (that.isDone()) { callback(); }
+        });
+        img.addEventListener("error", function () {
+            that.errorCount += 1;
+            if (that.isDone()) { callback(); }
+        });
+        img.src = path;
+        this.cache[path] = img;
+    }
+}
+
+AssetManager.prototype.getAsset = function (path) {
+    //console.log(path.toString());
+    return this.cache[path];
+}
+
+function Animation(spriteSheet, startX, startY, frameWidth, frameHeight, frameDuration, frames, loop, reverse) {
+    this.spriteSheet = spriteSheet;
+    this.startX = startX;
+    this.startY = startY;
+    this.frameWidth = frameWidth;
+    this.frameDuration = frameDuration;
+    this.frameHeight = frameHeight;
+    this.frames = frames;
+    this.totalTime = frameDuration * frames;
+    this.elapsedTime = 0;
+    this.loop = loop;
+    this.reverse = reverse;
+}
+
+Animation.prototype.drawFrame = function (tick, ctx, x, y, scaleBy) {
+    var scaleBy = scaleBy || 1;
+    this.elapsedTime += tick;
+    if (this.loop) {
+        if (this.isDone()) {
+            this.elapsedTime = 0;
+        }
+    } else if (this.isDone()) {
+        return;
+    }
+    var index = this.reverse ? this.frames - this.currentFrame() - 1 : this.currentFrame();
+    var vindex = 0;
+    if ((index + 1) * this.frameWidth + this.startX > this.spriteSheet.width) {
+        index -= Math.floor((this.spriteSheet.width - this.startX) / this.frameWidth);
+        vindex++;
+    }
+    while ((index + 1) * this.frameWidth > this.spriteSheet.width) {
+        index -= Math.floor(this.spriteSheet.width / this.frameWidth);
+        vindex++;
+    }
+
+    var locX = x;
+    var locY = y;
+    var offset = vindex === 0 ? this.startX : 0;
+    ctx.drawImage(this.spriteSheet,
+                  index * this.frameWidth + offset, vindex * this.frameHeight + this.startY,  // source from sheet
+                  this.frameWidth, this.frameHeight,
+                  locX, locY,
+                  this.frameWidth * scaleBy,
+                  this.frameHeight * scaleBy);
+}
+
+Animation.prototype.currentFrame = function () {
+    return Math.floor(this.elapsedTime / this.frameDuration);
+}
+
+Animation.prototype.isDone = function () {
+    return (this.elapsedTime >= this.totalTime);
+}
+
+function Timer() {
+    this.gameTime = 0;
+    this.maxStep = 0.05;
+    this.wallLastTimestamp = 0;
+}
+
+Timer.prototype.tick = function () {
+    var wallCurrent = Date.now();
+    var wallDelta = (wallCurrent - this.wallLastTimestamp) / 1000;
+    this.wallLastTimestamp = wallCurrent;
+
+    var gameDelta = Math.min(wallDelta, this.maxStep);
+    this.gameTime += gameDelta;
+    return gameDelta;
+}
+
+function GameEngine() {
+    this.entities = [];
+    this.ctx = null;
+    this.click = null;
+    this.mouse = null;
+    this.wheel = null;
+    this.surfaceWidth = null;
+    this.surfaceHeight = null;
+}
+
+GameEngine.prototype.init = function (ctx) {
+    this.ctx = ctx;
+    this.surfaceWidth = this.ctx.canvas.width;
+    this.surfaceHeight = this.ctx.canvas.height;
+    this.startInput();
+    this.timer = new Timer();
+    console.log('game initialized');
+}
+
+GameEngine.prototype.start = function () {
+    console.log("starting game");
+    var that = this;
+    (function gameLoop() {
+        that.loop();
+        requestAnimFrame(gameLoop, that.ctx.canvas);
+    })();
+}
+
+GameEngine.prototype.startInput = function () {
+    console.log('Starting input');
+
+    var getXandY = function (e) {
+        var x = e.clientX - that.ctx.canvas.getBoundingClientRect().left;
+        var y = e.clientY - that.ctx.canvas.getBoundingClientRect().top;
+
+        if (x < 1024) {
+            x = Math.floor(x / 32);
+            y = Math.floor(y / 32);
+        }
+
+        return { x: x, y: y };
+    }
+
+    var that = this;
+
+    this.ctx.canvas.addEventListener("click", function (e) {
+        that.click = getXandY(e);
+    }, false);
+
+    this.ctx.canvas.addEventListener("mousemove", function (e) {
+        that.mouse = getXandY(e);
+    }, false);
+
+    this.ctx.canvas.addEventListener("mousewheel", function (e) {
+        that.wheel = e;
+        e.preventDefault();
+    }, false);
+
+
+    this.ctx.canvas.addEventListener("keydown", function (e) {
+        if (e.keyCode === 39) {
+            that.rightArrow = true;
+        }
+        if (e.keyCode === 32) {
+            that.space = true;
+        }
+        e.preventDefault();
+    }, false);
+
+    this.ctx.canvas.addEventListener("keyup", function (e) {
+        if (e.keyCode === 39) {
+            that.rightArrow = false;
+            that.isRunKeyUp = true;
+        }
+        e.preventDefault();
+    }, false);
+
+
+    console.log('Input started');
+}
+
+GameEngine.prototype.addEntity = function (entity) {
+    console.log('added entity');
+    this.entities.push(entity);
+}
+
+GameEngine.prototype.draw = function (drawCallback) {
+    this.ctx.clearRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
+    this.ctx.save();
+    for (var i = 0; i < this.entities.length; i++) {
+        this.entities[i].draw(this.ctx);
+    }
+    if (drawCallback) {
+        drawCallback(this);
+    }
+    this.ctx.restore();
+}
+
+GameEngine.prototype.update = function () {
+    var entitiesCount = this.entities.length;
+
+    for (var i = 0; i < entitiesCount; i++) {
+        var entity = this.entities[i];
+
+        if (!entity.removeFromWorld) {
+            entity.update();
+        }
+    }
+
+    for (var i = this.entities.length - 1; i >= 0; --i) {
+        if (this.entities[i].removeFromWorld) {
+            this.entities.splice(i, 1);
+        }
+    }
+}
+
+GameEngine.prototype.loop = function () {
+    this.clockTick = this.timer.tick();
+    this.update();
+    this.draw();
+    this.isRunKeyUp = false;
+    this.rightArrow = null;
+    this.space = null;
+    this.click = null;
+    this.wheel = null;
+}
+
+function Entity(game, x, y) {
+    this.game = game;
+    this.x = x;
+    this.y = y;
+    this.removeFromWorld = false;
+}
+
+Entity.prototype.update = function () {
+}
+
+Entity.prototype.draw = function (ctx) {
+    if (this.game.showOutlines && this.radius) {
+        ctx.beginPath();
+        ctx.strokeStyle = "green";
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        ctx.stroke();
+        ctx.closePath();
+    }
+}
+
+Entity.prototype.rotateAndCache = function (image, angle) {
+    var offscreenCanvas = document.createElement('canvas');
+    var size = Math.max(image.width, image.height);
+    offscreenCanvas.width = size;
+    offscreenCanvas.height = size;
+    var offscreenCtx = offscreenCanvas.getContext('2d');
+    offscreenCtx.save();
+    offscreenCtx.translate(size / 2, size / 2);
+    offscreenCtx.rotate(angle);
+    offscreenCtx.translate(0, 0);
+    offscreenCtx.drawImage(image, -(image.width / 2), -(image.height / 2));
+    offscreenCtx.restore();
+    //offscreenCtx.strokeStyle = "red";
+    //offscreenCtx.strokeRect(0,0,size,size);
+    return offscreenCanvas;
+}
+
+// GameBoard code below
+
+function Background(game) {
+    Entity.call(this, game, 0, 400);
+}
+
+Background.prototype = new Entity();
+Background.prototype.constructor = Background;
+
+Background.prototype.update = function () {
+    Entity.prototype.update.call(this);
+}
+
+Background.prototype.draw = function (ctx) {
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 400, 1200, 300);
+    ctx.clearRect(0, 0, 1200, 435);
+
+}
+
+function RunBoy(game) {
+    this.standing = new Animation(ASSET_MANAGER.getAsset(heroSpriteSheet), 0, 0, 18, 35, 0.07, 1, true, false);
+    this.runAnimation = new Animation(ASSET_MANAGER.getAsset(heroSpriteSheet), 0, 0, 18, 35, 0.04, 12, true, false);
+    this.jumpAnimation = new Animation(ASSET_MANAGER.getAsset(heroSpriteSheet), 0, 96, 18, 29, 0.07, 18, false);
+    this.jumping = false;
+    this.running = false;
+
+    // set the sprite's starting position on the canvas
+    Entity.call(this, game, 20, 372);
+}
+
+RunBoy.prototype = new Entity();
+RunBoy.prototype.constructor = RunBoy;
+
+RunBoy.prototype.update = function () {
+
+    if (this.game.rightArrow && !this.game.isRunKeyUp) {
+        this.running = true;
+        this.x += 1;
+    }
+
+    if (this.game.isRunKeyUp) {
+        this.running = false;
+    }
+
+    if (this.game.space) {
+        this.jumping = true;
+        this.x += 10;
+    }
+    Entity.prototype.update.call(this);
+}
+
+RunBoy.prototype.draw = function (ctx) {
+
+    if (this.jumping) {
+        var height = 0;
+        var duration = this.jumpAnimation.elapsedTime + this.game.clockTick;
+        var maxHeight = 300;
+        if (duration > this.jumpAnimation.totalTime / 2) duration = this.jumpAnimation.totalTime - duration;
+        duration = duration / this.jumpAnimation.totalTime;
+        // linear jump
+        var height = maxHeight * 2 * duration + 17;
+
+        // quadratic jump
+        height = (4 * duration - 4 * duration * duration) * maxHeight + 17;
+
+        //this.jumpAnimation.drawFrame(this.game.clockTick, ctx, this.x + 32, this.y - height);
+        this.jumpAnimation.drawFrame(this.game.clockTick, ctx, this.x, this.y - height / 2);
+
+
+        if (this.jumpAnimation.isDone()) {
+            this.jumpAnimation.elapsedTime = 0;
+            this.jumping = false;
+        }
+
+    }
+    else if (this.running) {
+
+        this.runAnimation.drawFrame(this.game.clockTick, ctx, this.x, this.y);
+
+    } else {
+        this.standing.drawFrame(this.game.clockTick, ctx, this.x, this.y);
+    }
+}
+
+// the "main" code begins here
+
+var ASSET_MANAGER = new AssetManager();
+
+ASSET_MANAGER.queueDownload(heroSpriteSheet);
+
+ASSET_MANAGER.downloadAll(function () {
+
+    var canvas = document.getElementById('gameWorld');
+    canvas.setAttribute("tabindex", 0);
+    canvas.focus();
+    var ctx = canvas.getContext('2d');
+
+    var gameEngine = new GameEngine();
+    var bg = new Background(gameEngine);
+    var boy = new RunBoy(gameEngine);
+
+    gameEngine.addEntity(bg);
+    gameEngine.addEntity(boy);
+
+    gameEngine.init(ctx);
+    gameEngine.start();
+});
+
+
+
+/*
+ This game shell was happily copied from Googler Seth Ladd's "Bad Aliens" game and his Google IO talk in 2011
+
+heroSpriteSheet = "runBoy1.png";
+
+window.requestAnimFrame = (function () {
+    return window.requestAnimationFrame ||
+            window.webkitRequestAnimationFrame ||
+            window.mozRequestAnimationFrame ||
+            window.oRequestAnimationFrame ||
+            window.msRequestAnimationFrame ||
+            function (callback,   element) {
+                window.setTimeout(callback, 1000 / 60);
+            };
+})();
+
 function Animation(spriteSheet, startX, startY, frameWidth, frameHeight, frameDuration, frames, loop, reverse) {
     this.spriteSheet = spriteSheet;
     this.startX = startX;
@@ -67,6 +468,21 @@ AssetManager.prototype.getAsset = function (path) {
     return this.cache[path];
 }
 
+function Timer() {
+    this.gameTime = 0;
+    this.maxStep = 0.05;
+    this.wallLastTimestamp = 0;
+}
+
+Timer.prototype.tick = function () {
+    var wallCurrent = Date.now();
+    var wallDelta = (wallCurrent - this.wallLastTimestamp) / 1000;
+    this.wallLastTimestamp = wallCurrent;
+
+    var gameDelta = Math.min(wallDelta, this.maxStep);
+    this.gameTime += gameDelta;
+    return gameDelta;
+}
 
 function GameEngine() {
     this.entities = [];
@@ -231,6 +647,7 @@ Entity.prototype.rotateAndCache = function (image, angle) {
     return offscreenCanvas;
 }
 
+/*
 function Tile(x, y) {
     this.x = x;
     this.y = y;
@@ -260,7 +677,7 @@ GameBoard.prototype.update = function () {
 
 /*
 Method used to build the initial board.
-*/
+
 var initializeBoard = function () {
 
     var output = []
@@ -287,6 +704,7 @@ GameBoard.prototype.draw = function (ctx) {
         }
     }
 }
+
 
 function Background(game) {
     Entity.call(this, game, 0, 400);
@@ -332,7 +750,7 @@ Background.prototype.draw = function (ctx) {
         ctx.fillRect(0, h, WIDTH, 1);
     }
 }
-*/
+
 
 function Player(game) {
 

411d90d36079529b9fcc171040fbfe80833dd7d0
christopher-witcher <christopher.witcher@gmail.com>
2014-04-18 02:21:54 -0700

    Split Game.js

---
+++ Sibiling_Project_reboot/Sibiling_Project_reboot/Background.js
@@ -0,0 +1,23 @@
+﻿var backImg = "neighBackgroundext.png";
+
+
+function Background(game) {
+    Entity.call(this, game, 0, 0);
+}
+
+Background.prototype = new Entity();
+Background.prototype.constructor = Background;
+
+Background.prototype.update = function () {
+    Entity.prototype.update.call(this);
+}
+
+Background.prototype.draw = function (ctx) {
+    this.image = ASSET_MANAGER.getAsset(backImg);
+    try {
+        ctx.drawImage(this.image, 0, 0);
+    } catch (Exception) {
+        console.log(Exception.message);
+    }
+
+}
---
+++ Sibiling_Project_reboot/Sibiling_Project_reboot/RunBoy.js
@@ -0,0 +1,110 @@
+﻿heroSpriteSheet = "blue_player.png";
+
+function RunBoy(game) {
+
+    this.rightStanding = new Animation(ASSET_MANAGER.getAsset(heroSpriteSheet), 40, 330, 60, 120, 0.01, 1, true, false);
+    this.leftStanding = new Animation(ASSET_MANAGER.getAsset(heroSpriteSheet), 145, 330, 60, 120, 0.01, 1, true, false);
+
+    this.runRight = new Animation(ASSET_MANAGER.getAsset(heroSpriteSheet), 0, 0, 145, 145, 0.1, 6, true, false);
+    this.runLeft = new Animation(ASSET_MANAGER.getAsset(heroSpriteSheet), 0, 180, 145, 145, 0.1, 6, true, false);
+
+    this.jumpRight = new Animation(ASSET_MANAGER.getAsset(heroSpriteSheet), 510, 340, 70, 100, 0.5, 1, false);
+    this.jumpLeft = new Animation(ASSET_MANAGER.getAsset(heroSpriteSheet), 510, 340, 70, 100, 0.5, 1, false);
+    this.jumping = false;
+    this.running = false;
+    this.standing = true;
+
+    // set the sprite's starting position on the canvas
+    Entity.call(this, game, 20, 550);
+}
+
+RunBoy.prototype = new Entity();
+RunBoy.prototype.constructor = RunBoy;
+
+RunBoy.prototype.update = function () {
+
+    if (this.game.rightArrow) {
+
+        this.running = true;
+        this.standing = false;
+        direction = true;
+        this.x += 5;
+    }
+
+    if (this.game.leftArrow) {
+        this.running = true;
+        this.standing = false;
+        direction = false;
+        this.x -= 5;
+    }
+
+    if (this.running === false) {
+        this.standing = true;
+    }
+
+    if (this.game.space) {
+        this.jumping = true;
+        this.x += 10;
+    }
+    Entity.prototype.update.call(this);
+}
+
+RunBoy.prototype.draw = function (ctx) {
+
+    if (this.jumping) {
+
+        var height = 0;
+        var maxHeight = 300;
+
+        if (direction) {
+
+            var duration = this.jumpRight.elapsedTime + this.game.clockTick;
+            if (duration > this.jumpRight.totalTime / 2) duration = this.jumpRight.totalTime - duration;
+            duration = duration / this.jumpRight.totalTime;
+            // linear jump
+            height = maxHeight * 2 * duration + 17;
+
+            // quadratic jump
+            height = (4 * duration - 4 * duration * duration) * maxHeight + 17;
+            this.jumpRight.drawFrame(this.game.clockTick, ctx, this.x, this.y - height / 2);
+
+            if (this.jumpRight.isDone()) {
+                this.jumpRight.elapsedTime = 0;
+                this.jumping = false;
+            }
+
+        } else {
+            var duration = this.jumpLeft.elapsedTime + this.game.clockTick;
+            if (duration > this.jumpLeft.totalTime / 2) duration = this.jumpLeft.totalTime - duration;
+            duration = duration / this.jumpLeft.totalTime;
+            // linear jump
+            height = maxHeight * 2 * duration + 17;
+
+            // quadratic jump
+            height = (4 * duration - 4 * duration * duration) * maxHeight + 17;
+            this.jumpLeft.drawFrame(this.game.clockTick, ctx, this.x, this.y - height / 2);
+
+            if (this.jumpLeft.isDone()) {
+                this.jumpLeft.elapsedTime = 0;
+                this.jumping = false;
+            }
+        }
+
+    } else if (this.running && (this.game.isLeftArrowUp === false || this.game.isRightArrowUp === false)) {
+        //this.standing = false;
+        if (direction) {
+            this.runRight.drawFrame(this.game.clockTick, ctx, this.x, this.y);
+        } else {
+            this.runLeft.drawFrame(this.game.clockTick, ctx, this.x, this.y);
+        }
+
+    } else {
+
+        if (direction) {
+            this.rightStanding.drawFrame(this.game.clockTick, ctx, this.x, this.y);
+        } else {
+            this.leftStanding.drawFrame(this.game.clockTick, ctx, this.x, this.y);
+        }
+
+    }
+}

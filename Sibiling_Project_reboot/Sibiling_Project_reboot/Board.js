9c5a701e953a21c4ade81ddcc96354b23e62167b
christopher-witcher <christopher.witcher@gmail.com>
2014-05-02 18:40:10 -0700

    fix

---
+++ Sibiling_Project_reboot/Sibiling_Project_reboot/Board.js
@@ -0,0 +1,24 @@
+﻿var obstacles;
+var legos;
+
+function Board(game) {
+    this.backDrop = new Animation(ASSET_MANAGER.getAsset(backImg), 0, 0, 1036, 735, .01, 1, true, false);
+    Entity.call(this, game, 0, 0);
+    this.game = game;
+    this.obstacles = [];
+    this.legos = [];
+}
+
+Board.prototype = new Entity();
+Board.prototype.constructor = Board;
+
+Board.prototype.update = function () {
+
+    Entity.prototype.update.call(this);
+}
+
+Board.prototype.draw = function (ctx) {
+    this.image = ASSET_MANAGER.getAsset(backImg);
+    this.backDrop.drawFrame(this.game.clockTick, ctx, 0, 0);
+
+}

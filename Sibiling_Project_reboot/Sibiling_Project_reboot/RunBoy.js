ef5065758338fe5727cd9a4e832bf9837e8f3f45
kennes2 <darkfaerie001@gmail.com>
2014-05-20 11:23:01 -0700

    Now hits his head when running into a block.

--- Sibiling_Project_reboot/Sibiling_Project_reboot/RunBoy.js
+++ Sibiling_Project_reboot/Sibiling_Project_reboot/RunBoy.js
@@ -47,6 +47,7 @@ function RunBoy(game, canvasWidth, worldWidth) {
     this.currentPlatform = null;
     //keeps track of where the bounding box's bottom was before it changed. should be when falling.
     this.lastBottom = this.boundingbox.bottom;
+    this.lastTop = this.boundingbox.top;
 
     //stores character's rewindStack
     this.myRewindStack = [];
@@ -92,6 +93,7 @@ RunBoy.prototype.update = function () {
             this.baseHeight = this.y;
         }
         this.lastBottom = this.boundingbox.bottom;
+        this.lastTop = this.boundingbox.top;
         this.boundingbox = new BoundingBox(this.x, this.y, this.boundingbox.width, this.boundingbox.height);
 
     }
@@ -162,6 +164,7 @@ RunBoy.prototype.update = function () {
             this.y = tempY;
         }
         this.lastBottom = this.boundingbox.bottom;
+        this.lastTop = this.boundingbox.top;
         this.boundingbox = new BoundingBox(this.x, this.y, this.boundingbox.width, this.boundingbox.height);
 
         /*
@@ -206,6 +209,7 @@ RunBoy.prototype.update = function () {
             this.height = (4 * duration - 4 * duration * duration) * maxHeight + 17;
 
             this.lastBottom = this.boundingbox.bottom;
+            this.lastTop = this.boundingbox.top;
             this.y = this.baseHeight - this.height / 2;
 
             if (this.jumpLeft.isDone()) {
@@ -216,6 +220,21 @@ RunBoy.prototype.update = function () {
 
             this.boundingbox = new BoundingBox(this.x - moveDistance, this.y, this.boundingbox.width, this.boundingbox.height);
         }
+
+        if (this.landed) {
+            if (direction) {
+                this.jumpRight.elapsedTime = 0;
+                this.x = this.x - moveDistance;
+            }
+            else {
+                this.jumpLeft.elapsedTime = 0;
+                this.x = this.x + moveDistance;
+            }
+            this.baseHeight = this.y;
+            this.jumping = false;
+            this.y = tempY;
+        }
+
         this.game.space = false; //stop Runboy from jumping continuously
 
         /*
@@ -229,6 +248,7 @@ RunBoy.prototype.update = function () {
         var tempX = this.x;
         this.move();
         this.lastBottom = this.boundingbox.bottom;
+        this.lastTop = this.boundingbox.top;
         if (this.x > tempX) {
             this.boundingbox = new BoundingBox(this.x, this.y, this.boundingbox.width, this.boundingbox.height);
         } else {//for when the world x moves but running boy doesn't move?
@@ -246,6 +266,7 @@ RunBoy.prototype.update = function () {
         var tempX = this.x;
         this.move();
         this.lastBottom = this.boundingbox.bottom;
+        this.lastTop = this.boundingbox.top;
         if (this.x < tempX) {
             this.boundingbox = new BoundingBox(this.x, this.y, this.boundingbox.width, this.boundingbox.height);
         } else {//for when the world x moves but running boy doesn't move?
@@ -482,7 +503,11 @@ RunBoy.prototype.didICollide = function () {
                     this.baseHeight = this.y;
                 }
                 
-            } else if (this.canPass && (this.currentPlatform == null || entity.y < this.currentPlatform.y)) {
+            }
+            else if (entity.boundingBox.bottom < this.lastTop && !this.landed) {
+                this.landed = result;
+            }
+            else if (this.canPass && (this.currentPlatform == null || entity.y < this.currentPlatform.y)) {
                 this.canPass = !result;
             }
       
